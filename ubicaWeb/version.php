b89ba4fe96087e565509546e2bde0ad65a7085ce
jramal <jramal@wsistemas4>
2016-05-26 14:18:04 -0500

    para saber la version del php

---
+++ ubicaWeb/version.php
@@ -0,0 +1,7 @@
+<?php
+// Imprime ejemplo 'Versión actual de PHP: 5.3.8'
+echo 'Versión actual de PHP: ' . phpversion();
+
+// Imprime ejemplo '2.0' o nada si la extensión no está habilitada
+echo phpversion('tidy');
+?>
